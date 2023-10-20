adbce031a725c81e42d949522a9a470680df15cb
Pedro Lobregati <pedroarthurlb02@gmail.com>
2023-10-20 11:33:36 -0300

    2010

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,62 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  visor: string = '0';
+  valor1!: number;
+  valor2!: number;
+  operacao! : number;
   constructor() {}
 
+  adicionarNumero(valor : string){
+      if(this.visor.length == 1 && this.visor == '0' && valor !== '.'){
+        this.visor = valor;
+      }else{
+        this.visor += valor;
+    }
+  }
+
+  adicionarVirgula(){
+    if(!(this.visor.includes('.'))){
+      this.visor +='.';
+    }
+  }
+
+  zerar(){
+    this.visor = '0';
+  }
+
+  adicionarOperacao(valor: number){
+    this.operacao = valor;
+    this.valor1 = +this.visor;
+    this.zerar();
+  }
+
+  calcular(){
+    this.valor2 = +this.visor;
+    switch(this.operacao){
+      case 0:
+        this.visor = "" + (this.valor1 + this.valor2);
+        break;
+      case 1:
+        this.visor = "" + (this.valor1 - this.valor2);
+        break;
+      case 2: 
+        this.visor = "" + (this.valor1 * this.valor2);
+        break;
+      case 3: 
+        if(this.valor2 == 0)
+          this.visor = "Error";
+        else
+          this.visor = "" + (this.valor1 / this.valor2);
+        break;
+    }
+  }
+
+  calcularPorcentagem(){
+    this.visor = "" + (+this.visor / 100);
+  }
+
+  trocarSinal(){
+    this.visor = "" + (+this.visor * (-1));
+  }
 }
